c5328d504d04ee97a8edae83344cf911d3f11ca2
krishna ingle <inglekrishna05@gmail.com>
2024-08-17 23:56:39 +0530

    update new

--- app/src/main/java/com/example/career_company/Login_Page.kt
+++ app/src/main/java/com/example/career_company/Login_Page.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashScreen : AppCompatActivity() {
+class Login_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide();
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_login_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
